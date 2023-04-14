ba71bd2db44c3a80134e956d8fbd1ab01efb6b82
Suchitra Swain <suchitraswain.2012gmail.com>
2023-04-14 12:08:32 +0200

    backend url updated

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://threejs-ai-19v2.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
